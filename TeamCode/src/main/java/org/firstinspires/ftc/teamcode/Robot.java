7f39edcfa379fa69b7d8509d62da038f12333baa
ROS837 <116839185+ROS837@users.noreply.github.com>
2022-11-10 15:56:59 -0500

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class Robot {
     LinearOpMode ln;
@@ -9,8 +10,20 @@ public class Robot {
     DcMotor lF;
     String lFInit = "lF";
 
+    DcMotor lB;
+    String lBInit = "lB";
+
+    DcMotor rF;
+    String rFInit = "rF";
+
+    DcMotor rB;
+    String rBInit = "rB";
+
     public Robot(LinearOpMode ln){
         lF = ln.hardwareMap.dcMotor.get(lFInit);
+        lB = ln.hardwareMap.dcMotor.get(lBInit);
+        rF = ln.hardwareMap.dcMotor.get(rFInit);
+        rB = ln.hardwareMap.dcMotor.get(rBInit);
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp", group = "teleOp")
 public class teleOp extends LinearOpMode {  
@@ -13,10 +14,68 @@ public class teleOp extends LinearOpMode {
         waitForStart();
 
         while(opModeIsActive()){
-            if(gamepad1.a){
-                robot.lF.setPower(0.3);
+            if(gamepad1.a) {
+                robot.lF.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.rB.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.rF.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.lB.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.lF.setPower(1);
+                robot.lB.setPower(1);
+                robot.rF.setPower(1);
+                robot.rB.setPower(1);
             }else{
                 robot.lF.setPower(0);
+                robot.lB.setPower(0);
+                robot.rF.setPower(0);
+                robot.rB.setPower(0);
+            }
+
+            if(gamepad1.b) {
+                robot.lF.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.rB.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.rF.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.lB.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.lF.setPower(1);
+                robot.lB.setPower(1);
+                robot.rF.setPower(1);
+                robot.rB.setPower(1);
+            }else{
+                robot.lF.setPower(0);
+                robot.lB.setPower(0);
+                robot.rF.setPower(0);
+                robot.rB.setPower(0);
+            }
+
+            if(gamepad1.x) {
+                robot.lF.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.rB.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.rF.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.lB.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.lF.setPower(1);
+                robot.lB.setPower(1);
+                robot.rF.setPower(1);
+                robot.rB.setPower(1);
+            }else{
+                robot.lF.setPower(0);
+                robot.lB.setPower(0);
+                robot.rF.setPower(0);
+                robot.rB.setPower(0);
+            }
+
+            if(gamepad1.y) {
+                robot.lF.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.rB.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.rF.setDirection(DcMotorSimple.Direction.FORWARD);
+                robot.lB.setDirection(DcMotorSimple.Direction.REVERSE);
+                robot.lF.setPower(1);
+                robot.lB.setPower(1);
+                robot.rF.setPower(1);
+                robot.rB.setPower(1);
+            }else{
+                robot.lF.setPower(0);
+                robot.lB.setPower(0);
+                robot.rF.setPower(0);
+                robot.rB.setPower(0);
             }
         }
     }
